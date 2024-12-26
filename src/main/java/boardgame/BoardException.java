6e42bf2e3791a48fadd24bcc75507e6daa6fbf5e
Guilherme Weissheimer <guilhermeweissheimer.w1@gmail.com>
2024-12-26 17:10:32 -0300

    BoardExceprion

---
+++ src/main/java/boardgame/BoardException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author guilherme
+ */
+public class BoardException extends RuntimeException{
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
