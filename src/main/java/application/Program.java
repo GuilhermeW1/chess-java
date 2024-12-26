03d38ad80ab6f3ee0073612508cbe111a2ce40ea
Guilherme Weissheimer <guilhermeweissheimer.w1@gmail.com>
2024-12-26 16:19:18 -0300

    Fist class Position

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,12 +4,15 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author guilherme
  */
 public class Program {
     public static void main(String[] args) {
-        
+        Position p = new Position(1,2);
+        System.out.println(p);
     }
 }
